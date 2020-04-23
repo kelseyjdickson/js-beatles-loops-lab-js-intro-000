03aede4295c0121385ab51e044bb56fbe4ec686f
Kelsey Dickson <kelseymedved@gmail.com>
2020-04-23 18:36:39 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 function theBeatlesPlay(musicians,instruments){
   let results = [];
   for(let i = 0; i <musicians.length; i++){
-    results += `${musicians[i]} plays the ${instruments[i]}`
+    results += `${musicians[i]} plays the ${instruments[i].}`
   }
   return results;
 }
