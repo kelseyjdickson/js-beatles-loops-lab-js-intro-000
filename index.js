cd3f29db3f46954bf31c21b0610b50f5ecd620a1
Kelsey Dickson <kelseymedved@gmail.com>
2020-04-23 18:44:42 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 // add solution here
 function theBeatlesPlay(musicians,instruments){
-
+results = [;]
   for(let i = 0; i <musicians.length; i++){
-    let results = `${musicians[i]} plays ${instruments[i]}, `
+   results += `${musicians[i]} plays ${instruments[i]}, `
   }
   return results;
 }
